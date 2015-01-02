23b03a4ee68282dbba65d69ce97833344b8e066e
Will Sentance <william.sentance@gmail.com>
2015-01-01 21:02:41 -0800

    Queries database with params

--- modules/logic.js
+++ modules/logic.js
@@ -2,22 +2,31 @@ var db = require('./db')
 
 var logic = function(params){
   console.log('==params==', params)
+  console.log(db.Phrase)
+  db.Phrase.find({ $and: [{category: 'weather'}, {option: 'dry'}] }, function(err, phrases){
+        if (err) return console.error(err);
+        console.log(phrases);
+  })
   // console.log(db)
-  db.once('open', function(){
-    console.log('connection opened')
+  for (key in params){
+    console.log('***',key, params[key],'***')
+    category = key.toLowerCase();
+    option = params[key].toLowerCase();
+    console.log('===', category, option)
+    db.Phrase.find({ $and: [{category: category}, {option: option}]}, function(err, phrases){
+      if (err) return console.error(err);
+      console.log(phrases);
+    })
+  }
+  // db.once('open', function(){
+  //   console.log('connection opened')
     
-    // console.log(params[key])
+  //   // console.log(params[key])
     
-    // for (key in params){
-    //   console.log('***',key, params[key],'***')
-    //   db.Phrase.find({ $and: [{category: key}, {option: params[key]}] }, function(err, phrases){
-    //     if (err) return console.error(err);
-    //     console.log(phrases);
-    //   })
-    // }
-    console.log('reached inside logic.js')
+  //   // 
+  //   console.log('reached inside logic.js')
     
-  })  
+  // })  
 }
 
 // receive input paramaters
