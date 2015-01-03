0d60cb08d4c806c1c81e413214e70031f7e4c538
Will Sentance <william.sentance@gmail.com>
2015-01-02 18:47:23 -0800

    passes output sentence to get request

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.set('port', (process.env.PORT || 5000));
 
 app.get('/phrase', function(req, res){
   logic(req.query, function(outputSentence){
+    console.log(outputSentence)
     res.send(outputSentence)
   });
 })
